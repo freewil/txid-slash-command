7a6497c1eb74b81367ec2d1607d3553aaca507ba
Sean Lavine <freewil@users.noreply.github.com>
2016-04-09 22:08:59 -0700

    complete /txid feature

--- README.md
+++ README.md
@@ -1,20 +1,25 @@
-# Giphy Slash Command for Mixmax
+# Bitcoin /txid slash command for Mixmax
 
-This is an open source Mixmax Slash Command. See <http://sdk.mixmax.com/docs/tutorial-giphy-slash-command> for more information about how to use this example code in Mixmax.
+This web service will allow you to add a /txid <txid> command to Mixmax which
+will provide a simple graph of the outputs of a bitcoin transaction.
+
+![txid slash command demo](https://i.giphy.com/l0LJ1hdNlzOtRlG5q.gif)
+
+## External Dependencies
+You will need to make sure you have graphviz/dot installed to render the graphs.
+
+Mac:
+```
+brew install graphviz
+```
 
 ## Running locally
 
 1. Install using `npm install`
 2. Run using `npm start`
 
-To simulate locally how Mixmax calls the typeahead URL (to return a JSON list of typeahead results), run:
-
-```
-curl http://localhost:9145/typeahead?text=cats
-```
-
 To simulate locally how Mixmax calls the resolver URL (to return HTML that goes into the email), run:
 
 ```
-curl http://localhost:9145/resolver?text=cats
+curl http://localhost:9145/resolver?text=608a651b4ffeb5a0514cd9074206b1d37b459531a8c0772ee643c5ad88e6e7e0
 ```
--- api/resolver.js
+++ api/resolver.js
@@ -1,75 +1,63 @@
-var key = require('../utils/key');
 var sync = require('synchronize');
 var request = require('request');
-var _ = require('underscore');
-
+var graphviz = require('graphviz');
 
 // The API that returns the in-email representation.
 module.exports = function(req, res) {
-  var term = req.query.text.trim();
+  var text = req.query.text.trim();
 
-  if (/^http:\/\/giphy\.com\/\S+/.test(term)) {
-    // Special-case: handle strings in the special URL form that are suggested by the /typeahead
-    // API. This is how the command hint menu suggests an exact Giphy image.
-    handleIdString(term.replace(/^http:\/\/giphy\.com\//, ''), req, res);
-  } else {
-    // Else, if the user was typing fast and press enter before the /typeahead API can respond,
-    // Mixmax will just send the text to the /resolver API (for performance). Handle that here.
-    handleSearchString(term, req, res);
+  var matches = text.match(/([0-9a-f]+)$/);
+  if (!matches) {
+    res.status(400).send('Invalid txid');
+    return;
   }
+
+  var txid = matches[1];
+
+  // get transaction json data from blockchain.info
+  var response;
+  try {
+    response = sync.await(request({
+      url: 'https://blockchain.info/tx-index/' + encodeURIComponent(txid),
+      qs: {
+        format: 'json'
+      },
+      gzip: true,
+      json: true,
+      timeout: 15 * 1000
+    }, sync.defer()));
+  } catch (e) {
+    res.status(500).send('Error');
+    return;
+  }
+
+  // get total input amount in BTC
+  var totalIn = response.body.inputs
+    .map(function(input) { return input.prev_out.value })
+    .reduce(function(a, b) { return a + b });
+  totalIn /= 1e8;
+
+  // create graph for the transaction
+  var g = graphviz.digraph('G');
+
+  // create input node for transaction graph
+  g.set('labelloc', 't');
+  g.set('label', txid);
+  var inNode = g.addNode(totalIn + ' BTC');
+
+  // create graph node and edge for each output
+  response.body.out.forEach(function(output) {
+    var btc = output.value / 1e8;
+    var outNode = g.addNode(btc + ' BTC');
+    g.addEdge(inNode, outNode);
+  });
+
+  // render svg to replace link
+  g.render('svg', function(buf) {
+    res.json({
+      body: buf.toString('utf8')
+    });
+  }, function(err) {
+    res.status(500).send('Error');
+  });
 };
-
-function handleIdString(id, req, res) {
-  var response;
-  try {
-    response = sync.await(request({
-      url: 'http://api.giphy.com/v1/gifs/' + encodeURIComponent(id),
-      qs: {
-        api_key: key
-      },
-      gzip: true,
-      json: true,
-      timeout: 15 * 1000
-    }, sync.defer()));
-  } catch (e) {
-    res.status(500).send('Error');
-    return;
-  }
-
-  var image = response.body.data.images.original;
-  var width = image.width > 600 ? 600 : image.width;
-  var html = '<img style="max-width:100%;" src="' + image.url + '" width="' + width + '"/>';
-  res.json({
-    body: html
-    // Add raw:true if you're returning content that you want the user to be able to edit
-  });
-}
-
-function handleSearchString(term, req, res) {
-  var response;
-  try {
-    response = sync.await(request({
-      url: 'http://api.giphy.com/v1/gifs/random',
-      qs: {
-        tag: term,
-        api_key: key
-      },
-      gzip: true,
-      json: true,
-      timeout: 15 * 1000
-    }, sync.defer()));
-  } catch (e) {
-    res.status(500).send('Error');
-    return;
-  }
-
-  var data = response.body.data;
-
-  // Cap at 600px wide
-  var width = data.image_width > 600 ? 600 : data.image_width;
-  var html = '<img style="max-width:100%;" src="' + data.image_url + '" width="' + width + '"/>';
-  res.json({
-    body: html
-    // Add raw:true if you're returning content that you want the user to be able to edit
-  });
-}
